203fe12e7b889839abfedc54e18acf100d89c2d5
Dandy Antariksa <40420198+Dandy22@users.noreply.github.com>
2023-03-20 13:11:31 +0700

    Add files via upload

---
+++ HASH_TABLE_lat_2.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct Node{
+	char nama[50];
+	int nilai;
+	struct Node *next;
+	
+}*head[26], *temp, *curr;
+
+void insertNode(char nama[], int nilai){
+	int index = nama[0] - 'A';
+	
+	temp = (struct Node *) malloc (sizeof(struct Node));
+	strcpy(temp->nama, nama);
+	temp->nilai = nilai;
+	
+	if(head[index] == NULL){
+		head[index] = temp;
+	}else{
+		int counter = 0;
+		while(head[index]!=NULL){
+			index++;
+			index%=26;
+			counter++;
+			if(counter>=26) return;
+		}
+		head[index] = temp;
+	}
+}
+
+void deleteChar(char nama[]){
+	for(int i = 0; i < 26; i++){
+		if(head[i] != NULL){
+			if(strcmp(head[i]->nama,nama) == 0){
+				free(head[i]);
+				head[i] = NULL;
+			}
+		}
+	}
+}
+
+void display(){
+	for(int i = 0; i < 26; i++){
+		if(head[i]!=NULL){
+				printf("[%d] - %s - %d\n", i, head[i]->nama, head[i]->nilai);
+		}
+	}
+}
+
+int main(){
+	
+	insertNode("Dandy", 20);
+	insertNode("Denis", 12);
+	insertNode("Zebra", 22);
+	insertNode("Zenis", 23); // karena sudah ada zebra sehingga ditaruh ke paling atas jika terjadi collision
+	display();
+	
+}
